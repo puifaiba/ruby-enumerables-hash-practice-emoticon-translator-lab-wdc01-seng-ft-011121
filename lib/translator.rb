be727afc6822da681ebc61cc4beb533112ebdb10
Boonie Attawut <puifai.ba@gmail.com>
2021-01-07 21:30:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,12 @@ def load_library(file_path)
   emoticons = {:english => {}, :japanese => {}}
   emoji_library.each do |meaning, emoji_array|
     emoticons[meaning] = emoticons
-    emoji_array.each do |emoji|
-      emoji[0] = emoticons[:english]
-      emoji[1] = emoticons[:japanese]
+    emoji_array.each_with_index do |emoji, index|
+      if index == 0
+        emoji = emoticons[:english]
+      else
+        emoji = emoticons[:japanese]
+      end
     end
   end
   emoticons
