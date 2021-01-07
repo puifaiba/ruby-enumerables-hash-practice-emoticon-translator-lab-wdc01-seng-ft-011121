3c1b3cd4494467d05dda092ff644c793ee4675e1
Boonie Attawut <puifai.ba@gmail.com>
2021-01-07 21:13:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,18 @@
 # require modules here
+require 'yaml'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoji_library = YAML.load_file(file_path)
+  emoticons = {:english => {}, :japanese => {}}
+  emoji_library.each do |meaning, emojis|
+    emoticons[:english][emojis[1]] = meaning
+    emoticons[:japanese][emojis[0]] = emojis[1]
+  end
+  emoticons
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
