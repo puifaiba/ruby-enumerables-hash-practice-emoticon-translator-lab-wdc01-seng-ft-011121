e157f8fc6695159d4a85633104a20a19a739bc2b
Boonie Attawut <puifai.ba@gmail.com>
2021-01-08 02:35:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,15 +13,14 @@ def load_library(file_path)
   emoticons
 end
 
+def get_english_meaning(file_path, emoticon)
+  # code goes here
+  load_library(file_path)
+end
 
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   load_library(file_path)
 
-end
-
-
-def get_english_meaning(file_path, emoticon)
-  # code goes here
 end
